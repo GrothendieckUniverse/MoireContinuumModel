6c54ab11c32274cf0b0488ffbacfcb56349b150c
xiaodong-hu <hxd.phys@gmail.com>
2025-09-28 15:18:45 -0700

    fix module  api change

--- src/MoireContinuumModel.jl
+++ src/MoireContinuumModel.jl
@@ -1,8 +1,8 @@
 module MoireContinuumModel
 
-include("r_stack_model.jl")
+include("r_stack_TMD_model.jl")
 using .RStackMoireContinuumModel
 export R_Stack_Twisted_Moire_Model, params_FengchengWu, params_ChongWang
-export initialize_r_stack_moire_continuum_model, plot_moire_bands
+export initialize_r_stack_moire_continuum_model, plot_moire_bands, plot_Ωxy_and_trace_G
 
 end # module MoireContinuumModel
